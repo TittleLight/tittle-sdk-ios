79a631b69c5bbb3c90e7934ad87bf20d22780a5a
Jackie <zhengjiekun@gmail.com>
2018-08-30 17:04:15 +0800

    setup project

---
+++ TittleFramework/HelloLogger.m
@@ -0,0 +1,15 @@
+//
+//  HelloLogger.m
+//  TittleFramework
+//
+//  Created by Jackie on 30/8/2018.
+//  Copyright © 2018 clarityhk.com. All rights reserved.
+//
+
+#import "HelloLogger.h"
+
+@implementation HelloLogger
+- (void)helloWithText:(NSString *)text {
+    NSLog(@"Hello, %@", text);
+}
+@end
